4f79ad1d0586714c181901c5756b79d39580afa4
davimdantas <davimdantas@gmail.com>
2020-10-10 20:50:54 -0300

    update repository

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Heading from './components/heading/heading';
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+
+const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+helloWorldButton.render();
+heading.render();
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
+      // {
+      //     test: /\.css$/,
+      //     use: ['style-loader', 'css-loader']
+      // },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['transform-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new CleanWebpackPlugin()
+  ],
+};
