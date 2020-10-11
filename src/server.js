7929f692d0bc2a9701e92a3e713710f7f2919183
davimdantas <davimdantas@gmail.com>
2020-10-11 18:14:23 -0300

    40: serve static files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-console */
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Some dummy content');
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
 });
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
 app.listen(3000, () => {
   console.log('Application is running on localhost:3000');
