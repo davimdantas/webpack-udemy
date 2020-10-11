b1924a8eaa72f2111d5b80ddf1cc128a1134dfac
davimdantas <davimdantas@gmail.com>
2020-10-11 17:04:44 -0300

    31: multiple bundle

---
+++ src/kiwi.js
@@ -0,0 +1,6 @@
+import Heading from './components/heading/heading';
+import KiwiImage from './components/kiwi-image/kiwi-image';
+
+const heading = new Heading();
+heading.render();
+KiwiImage.render();
