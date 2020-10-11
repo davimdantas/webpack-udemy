e13bff29ac100b2ee3229c5733550f4d02cde1d2
davimdantas <davimdantas@gmail.com>
2020-10-11 19:18:59 -0300

    47: bootstrap

--- src/hello-world.js
+++ src/hello-world.js
@@ -2,6 +2,8 @@
 import Heading from './components/heading/heading';
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import './index.scss';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
